cb03cc69eb8576b2cd5a2a87edcd4319f4e9d9d5
Victor Lopez <victor.lopez@pal-robotics.com>
2019-05-14 10:22:22 +0200

    Update PAL licenses

--- include/gpd_utils/bt_actions/object_cloud_extraction_action.h
+++ include/gpd_utils/bt_actions/object_cloud_extraction_action.h
@@ -1,3 +1,11 @@
+/*
+ * Copyright 2019 PAL Robotics SL. All Rights Reserved
+ *
+ * Unauthorized copying of this file, via any medium is strictly prohibited,
+ * unless it was supplied under the terms of a license agreement or
+ * nondisclosure agreement with PAL Robotics SL. In this case it may not be
+ * copied or disclosed except in accordance with the terms of that agreement.
+ */
 #ifndef OBJECT_CLOUD_EXTRACTION_ACTION_H
 #define OBJECT_CLOUD_EXTRACTION_ACTION_H
 
